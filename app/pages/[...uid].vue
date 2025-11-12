367fd543bffe05702d0fcb26c81fbfd581725d5f
timothe joubert <timothejoubert26@gmail.com>
2025-11-12 21:01:14 +0100

    fix: fix sliceZone components import

--- app/pages/[...uid].vue
+++ app/pages/[...uid].vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import type { AboutPageDocument, ArchivePageDocument, HomePageDocument, ProjectListingPageDocument, ProjectPageDocument } from '~~/prismicio-types'
+import { components } from '~/components/slices/index'
 
 const { document } = await useFetchPage(undefined)
 
@@ -8,30 +9,39 @@ const aboutDocument = computed(() => document.value.type === 'about_page' ? docu
 const archiveDocument = computed(() => document.value.type === 'archive_page' ? document.value as ArchivePageDocument : undefined)
 const projectListingDocument = computed(() => document.value.type === 'project_listing_page' ? document.value as ProjectListingPageDocument : undefined)
 const projectDocument = computed(() => document.value.type === 'project_page' ? document.value as ProjectPageDocument : undefined)
+
+console.log(document.value)
 </script>
 
 <template>
-	<LazyVHomePage
-		v-if="homeDocument"
-		:document="homeDocument"
-	/>
-	<LazyVProjectPage
-		v-else-if="projectDocument"
-		:document="projectDocument"
-	/>
-	<LazyVProjectListingPage
-		v-else-if="projectListingDocument"
-		:document="projectListingDocument"
-	/>
-	<LazyVArchivePage
-		v-else-if="archiveDocument"
-		:document="archiveDocument"
-	/>
-	<LazyVAboutPage
-		v-else-if="aboutDocument"
-		:document="aboutDocument"
-	/>
-	<div v-else>
-		<h1>Fallback page</h1>
+	<div>
+		<LazyVHomePage
+			v-if="homeDocument"
+			:document="homeDocument"
+		/>
+		<LazyVProjectPage
+			v-else-if="projectDocument"
+			:document="projectDocument"
+		/>
+		<LazyVProjectListingPage
+			v-else-if="projectListingDocument"
+			:document="projectListingDocument"
+		/>
+		<LazyVArchivePage
+			v-else-if="archiveDocument"
+			:document="archiveDocument"
+		/>
+		<LazyVAboutPage
+			v-else-if="aboutDocument"
+			:document="aboutDocument"
+		/>
+		<div v-else>
+			<h1>Fallback page</h1>
+		</div>
+		<LazySliceZone
+			v-if="document.data.slices?.length"
+			:slices="document.data.slices"
+			:components="components"
+		/>
 	</div>
 </template>
--- app/pages/slice-simulator.vue
+++ app/pages/slice-simulator.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { SliceSimulator } from '@slicemachine/adapter-nuxt/simulator'
-import { components } from '~/slices'
+import { components } from '~/components/slices/index'
 </script>
 
 <template>
